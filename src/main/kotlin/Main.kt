8b3034b5590597852f4ea6f0d479f3af19d26f7f
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-06-15 15:21:01 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+package world.anhgelus.lemonde-live-discord
+
+
+fun main() {
+    val name = "Kotlin"
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
